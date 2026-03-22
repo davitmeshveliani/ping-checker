84e2a4ce48385ac71179b48b8aa77f5321242eba
davitmeshveliani <davitmeshveliani979@gmail.com>
2026-03-22 19:16:10 +0100

    Added German ping monitor script

---
+++ ping_test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+read -p "Введите адрес: " host
+fails=0
+
+for i in {1..20}; do
+  r=$(ping -n 1 $host 2>&1)
+
+  
+  if ! echo "$r" | grep -qiE "Antwort|Zeit"; then
+    ((fails++))
+    echo "[$i] Fail ($fails/3)"
+    if [ "$fails" -ge 3 ]; then
+      echo "🚨 3 подряд пинга не удались"
+      fails=0
+    fi
+  else
+    fails=0
+   
+    t=$(echo "$r" | grep -ao "Zeit=[0-9]*" | cut -d'=' -f2)
+    
+    if [ -n "$t" ] && [ "$t" -gt 100 ] 2>/dev/null; then
+      echo "[$i]  Слишком медленно: ${t}ms"
+    else
+      echo "[$i] OK: ${t:-0}ms"
+    fi
+  fi
+  sleep 1
+done
+echo "Тест завершен."
